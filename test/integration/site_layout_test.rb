c113ec053534d02ff0502ace6ac54ad46bcefe30
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-08 01:36:31 +0530

    changed site layout test.rb for signup page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,5 +8,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    get signup_path
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
   end
 end
