2c388e1d683ae0c688af8a63fffd7996a754c2c5
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-09 00:27:44 +0530

    changed model to add validation length

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   validates(:name, {presence:true})
 
   validates(:email, {presence:true})
-
+  validates :name, length {maximum:50}
+  validates :email, length {maximum:256}
 end
