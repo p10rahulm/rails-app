394f1afc4f3967a94e356b9c264fcfb8c09a26ee
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-09 00:54:05 +0530

    added some more validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
   validates(:email, {presence:true})
   validates :name, length: {maximum:50}
   validates :email, length: {maximum:255}
+  validates :email, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format:{with: VALID_EMAIL_REGEX }
+
+
 end
