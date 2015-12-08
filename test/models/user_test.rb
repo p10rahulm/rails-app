f33e284ce53c3de365cac45daa6a19bd42446789
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-09 00:25:56 +0530

    changed tests to add length check on user and email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,17 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "   "
     assert_not @user.valid?
   end
+  test "email should be present" do
+    @user.email = "   "
+    assert_not @user.valid?
+  end
+  test "name is not too long" do
+    @user.name = "a"*51
+    assert_not  @user.valid?
+  end
+  test "email is not too long" do
+    @user.email = "a"*244+"@example.com"
+    assert_not @user.valid?
+  end
+
 end
