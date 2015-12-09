41c59d7baa0d313d7e1febf6132f4a70523d04b1
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-10 04:12:22 +0530

    changed user model for pwrd validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ActiveRecord::Base
   validates :email, format:{with: VALID_EMAIL_REGEX }
 
   has_secure_password
+  validates :password, length: {minimum:6}
+  validates :password_confirmation, length: {minimum:6}
+  validates :password, {presence:true}
+  validates :password_confirmation, {presence:true}
 end
