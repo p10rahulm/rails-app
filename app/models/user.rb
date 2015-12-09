0638db04ffadba5330312cacea9830f0ced1e023
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-10 03:00:45 +0530

    Users uniqueness case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   validates :name, length: {maximum:50}
   validates :email, length: {maximum:255}
   validates :email, uniqueness: true
+  validates :email, uniqueness: { case_sensitive: false }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format:{with: VALID_EMAIL_REGEX }
 
