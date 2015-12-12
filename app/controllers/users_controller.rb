608d3ec1a1a133a1fc97180355075f973e3ee275
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-12 16:20:41 +0530

    added create def to user controller with private user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user]) # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
 # Handle a successful save.
     else
@@ -17,4 +17,8 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
