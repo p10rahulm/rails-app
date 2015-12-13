9265a0e5dcd65e159f3a2cdb10c9c418787fcb88
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-13 13:48:04 +0530

    added a flash warning in case failed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-# Log the user in and redirect to the user's show page.
+      # Log the user in and redirect to the user's show page.
     else
-# Create an error message.
+      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      # Create an error message.
       render 'new'
     end
 
