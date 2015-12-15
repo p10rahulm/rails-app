47435ccb5d440aa2847388df0ac0ff81646f43fe
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-15 17:44:46 +0530

    add user.digest method for user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,11 @@ class User < ActiveRecord::Base
   validates :password_confirmation, length: {minimum:6}
   validates :password, {presence:true}
   validates :password_confirmation, {presence:true}
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 end
