e4a837387f69353ab3742ecad9d22533040a8dc7
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-18 16:51:58 +0530

    create index method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
   def show
@@ -9,6 +9,9 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+  def index
+    @users = User.all
   end
 
   def create
