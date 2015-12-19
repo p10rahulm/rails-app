8dedb93c41c4ab84643670efd5c44601996daa9c
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-19 10:32:13 +0530

    added params for test environment in config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,14 @@ Rails.application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
+
+  #Some config to view mails on url
+  # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'
+  # config.action_mailer.default_url_options = { host: host }
+
+
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
