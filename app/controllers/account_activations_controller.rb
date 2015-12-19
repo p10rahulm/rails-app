b61a365fde008753b4c6b51e83def52202e87ebd
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-19 11:13:20 +0530

    created edit method in account_activations controller

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,18 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    @user = User.find_by(email: params[:email])
+    if @user && !@user.activated? && @user.authenticated?(:activation, params[:id])
+      @user.update_attribute(:activated, true)
+      @user.update_attribute(:activated_at, Time.zone.now)
+      log_in @user
+      flash[:success] = "Account activated! Welcome to the Sample App!"
+      redirect_to @user
+      # equivalent to redirect_to user_url(@user)
+
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,7 @@ class UsersController < ApplicationController
       UserMailer.account_activation(@user).deliver_now
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
-      # log_in @user
-      # flash[:success] = "Welcome to the Sample App!"
-      # redirect_to @user
-      # equivalent to redirect_to user_url(@user)
+
     else
       render 'new'
     end
