e784cbada54189d28be4764df93911e64054c4be
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-19 10:42:04 +0530

    added some tests and changed test environment config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,10 +9,10 @@ Rails.application.configure do
 
 
   #Some config to view mails on url
-  # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
   host = 'localhost:3000'
-  # config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host }
 
 
   # Do not eager load code on boot. This avoids loading your whole application
