9584671fd5d734354d0ea4fca575d1b8fee854c2
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-19 12:50:09 +0530

    added some defs to user model to remove from user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,16 @@ class User < ActiveRecord::Base
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
-    end
+  end
+  # Activates an account.
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
   private
   # Converts email to all lower-case.
