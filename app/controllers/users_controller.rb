7b08680eddc963453088a23b174b1e737bf3591f
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-20 12:32:21 +0530

    added to users_controller to show on page and index only activated users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated == true
+    # redirect_to root_url and return unless @user.activated?
     # debugger
   end
 
@@ -12,7 +14,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
+    # @users = User.paginate(page: params[:page])
   end
 
   def create
