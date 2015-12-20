b4db9644bbbb258eaf873fe118664dd2e6c3af7e
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-20 12:59:05 +0530

    activate and create reset digest methods to hit db only once

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,8 +45,9 @@ class User < ActiveRecord::Base
   end
   # Activates an account.
   def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at:Time.zone.now)
+    # update_attribute(:activated, true)
+    # update_attribute(:activated_at, Time.zone.now)
   end
   # Sends activation email.
   def send_activation_email
@@ -56,8 +57,9 @@ class User < ActiveRecord::Base
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+    # update_attribute(:reset_digest, User.digest(reset_token))
+    # update_attribute(:reset_sent_at, Time.zone.now)
   end
   # Sends password reset email.
   def send_password_reset_email
