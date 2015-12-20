fcca35f932bbea2d5947c89aff114044e0b0bae3
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-21 02:04:42 +0530

    changed carrier_wave file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ if Rails.env.production?
       :aws_secret_access_key => "EH/lQzMssoyK3vk+bHhJHFoiW3g9WmON00vyKocw",
       :region                 => "ap-southeast-1"
     }
-    config.fog_directory = ENV['rubyjolapp']
+    config.fog_directory = "rubyjolapp"
     config.fog_public     = false                                   # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
     end
