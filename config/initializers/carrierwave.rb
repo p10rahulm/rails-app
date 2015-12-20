5abf672def50c2fe5545602b62dfbffb0a05db79
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-21 02:02:53 +0530

    changed carrierwave file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,16 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.root = Rails.root.join('tmp') # adding these...
-    config.cache_dir = 'carrierwave' # ...two lines
+    config.root = Rails.root.join('tmp')
+    config.cache_dir = 'carrierwave'
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['AKIAJ4IUIOTMD556KNHA'],
-      :aws_secret_access_key => ENV['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'],
-      :region                 => 'ap-southeast-1'
+      :provider => "AWS",
+      :aws_access_key_id => "AKIAJ4IUIOTMD556KNHA",
+      :aws_secret_access_key => "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",
+      :region                 => "ap-southeast-1"
     }
     config.fog_directory = ENV['rubyjolapp']
     config.fog_public     = false                                   # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-
-  end
+    end
 end
