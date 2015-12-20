33329b998908fedee99eef0d0d0ef76dd2c55373
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-20 18:11:03 +0530

    generated tests for microcontroller interface

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class MicropostsControllerTest < ActionController::TestCase
   def setup
     @micropost = microposts(:orange)
+    user1 = users(:rahul)
   end
 
   test "should redirect create when not logged in" do
@@ -11,12 +12,20 @@ class MicropostsControllerTest < ActionController::TestCase
     end
     assert_redirected_to login_url
   end
-  
+
   test "should redirect destroy when not logged in" do
     assert_no_difference 'Micropost.count' do
       delete :destroy, id: @micropost
     end
     assert_redirected_to login_url
+  end
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(:rahul)
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete :destroy, id: micropost
+    end
+    assert_redirected_to root_url
   end
 end
 
