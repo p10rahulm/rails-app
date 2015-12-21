e61e8196463877054b61f7df1ab48e008ed4bace
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-21 12:15:26 +0530

    added test for picture uploads

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -31,6 +31,25 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     get user_path(users(:hombalappa))
     assert_select 'a', text: 'delete', count: 0
   end
+  test "micropost interface for picture uploads" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    assert_select 'input[type=file]'
+    # Valid submission with picture
+    content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, micropost: { content: content, picture: picture }
+    end
+    assert assigns(:micropost).picture?
+
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body
+
+  end
+
   test "micropost sidebar count" do
     log_in_as(@user)
     get root_path
