04e3dd06f9a2c60b648f7659e11ebb8bed5cdea3
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-21 22:08:00 +0530

    changed static bits to 5S shingo

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 class ApplicationHelperTest < ActionView::TestCase
   test "fill title helper"do
-    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title, "Shingo Institute 5S App"
+    assert_equal full_title("Help"), "Help | Shingo Institute 5S App"
   end
 
 end
