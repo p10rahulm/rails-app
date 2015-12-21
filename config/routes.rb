9e68382dc215927f6df6d3796337356604a0d8a7
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-21 14:50:51 +0530

    added followers and following routes for users resource

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,11 @@
   delete 'logout' => 'sessions#destroy'
 
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts,only: [:create, :destroy]
