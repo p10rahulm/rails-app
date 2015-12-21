b319f67636c4a575a065f4d4fd8a36a1c9190925
Rahul Madhavan <rahul.maddy@gmail.com>
2015-12-21 15:05:30 +0530

    added relationships resource to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts,only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
 end
 
